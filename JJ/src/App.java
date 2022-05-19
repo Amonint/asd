ce163f5205a10ec812f9bedc0e0fe3aa857a932e
payala909@gmail.com <payala909@gmail.com>
2022-05-19 18:09:49 -0500

    ,,

---
+++ JJ/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x[][]={{3,3,3},{3,3,4}};
+       
+
+        for (int i = 0; i < x.length; i++) {
+            for (int j = 0; j < x[0].length; j++) {
+                System.out.println(x[i][j]);
+                
+            }
+        }
+       
+    }
+    
+}
+
---
+++ JJ/src/problema4.java
@@ -0,0 +1,18 @@
+/* public class problema2 {
+    public static void main(String[] args) throws Exception {
+        int[] filas = {3,2,3};
+        int[] columnas = {3,2,3,5};
+        int AP=0,Ai=0;
+
+        for (int i = 0; x < filas.length; i++) {
+            for (int j = 0; x < columnas[0].length; j++) {
+                if(x[i][j]%2=0);
+                    Ap+=x[i][j];
+                else
+                    Ai=x[i][j];
+            }
+        }
+        
+        System.out.println("");
+    }
+} */
